f68cb3db62f887c735c5874ade92b2b08c2df7bb
Pingoda <francescoclem915@gmail.com>
2025-10-16 10:05:05 +0200

    Initial Commit

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,13 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Link } from 'expo-router';
 
-export default function Index() {
+export default function NotFound() {
   return (
-    <View style = { styles.container } >
-      <Text style = { styles.text } >Home Screen</Text>
-      <Link href='/about' style = { styles.button } >
-        Go to About Screen
-      </Link>
+    <View style = { styles.container }>
+      <Text style = { styles.text }>Error 404: Page not found</Text>
+      <Link href='/(tabs)/index' style = { styles.button }>Go back to Home Page</Link>
     </View>
   );
 }
